a4abf23ba3ac47ef62c40b14118462ce92a3b946
sedgar03 <steven.edgar3@gmail.com>
2024-08-28 10:37:49 -0700

    Commiting a small amount of stuff

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Start the backend
 cd backend
 pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port 8000 &
+uvicorn main:app --host 0.0.0.0 --port 8080 &
 
 # Start the frontend
 cd ../frontend
