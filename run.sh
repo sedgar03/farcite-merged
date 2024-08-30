ba32df7f1566f7a24af732afb8a1fcb0314aaa6d
sedgar03 <131286656+sedgar03@users.noreply.github.com>
2024-08-30 00:52:47 +0000

    Working with port 80

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Build and start the frontend
-cd frontend
-npm install
-npm run build
-npx serve -s build -l 80 &
-
 # Start the backend
-cd ../backend
+cd backend
 pip install -r requirements.txt
 uvicorn main:app --host 0.0.0.0 --port 8080 &
+
+# # Build and start the frontend
+cd ../frontend
+npm install
+npm run build
+npx serve -s build -l 3000
+
+
